98400ab27fac10c3784cf6e092c4254d6a4ff5d7
Fred <frederic@pmo.digital>
2020-02-11 20:02:00 +0100

    3: Mise en page des formulaires inscription et login

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,45 +3,58 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+use App\Form\ApplicationType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
-class RegistrationFormType extends AbstractType
+class RegistrationFormType extends ApplicationType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+            ->add('email', EmailType::class, $this->getConfiguration("E-mail", "Votre adresse électronique ...", [
+                'required'=>false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez rentrer une adresse e-mail valide',
+                    ])
+                ]   
+            ]))
+            ->add('firstName', TextType:: class, $this->getConfiguration("Prénom", "Votre prénom ...", ['required'=>false])  )
+            ->add('lastName', TextType:: class, $this->getConfiguration("Nom", "Votre nom ...", ['required'=>false]) )
+            ->add('agreeTerms', CheckboxType::class, $this->getConfiguration("J'accepte les conditions générales", "", [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Veillez accepter les conditions générales.',
                     ]),
                 ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
+            ]))
+            ->add('plainPassword', PasswordType::class, $this->getConfiguration("Mot de passe", "Choisissez un mot de passe ..." , [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'required'=>false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Renseignez un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mote de passe doit contenir {{ limit }} charactères minimum',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
-            ])
+            ]))
         ;
     }
 
