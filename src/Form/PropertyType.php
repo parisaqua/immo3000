ce7b1b1d39f13fce1b0d845ec0906b53b04697ee
Fred <frederic@pmo.digital>
2020-02-26 16:00:56 +0100

    34: upload image 13:56 mn

--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
@@ -31,12 +32,16 @@ class PropertyType extends AbstractType
             ->add('options', EntityType::class, [
                 'class' => Option::class,
                 'choice_label' => 'name',
-                'multiple' => true
+                'multiple' => true,
+                'required' => false
             ])
             ->add('address')
             ->add('postalCode')
             ->add('sold')
             ->add('city')
+            ->add('imageFile', FileType::class, [
+                'required' => false
+            ])
         ;
     }
 
